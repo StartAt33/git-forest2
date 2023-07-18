36d521c232fbc8a8a353b693b3fb432c3383d646
Forest huang <frank.bin.huang@gmail.com>
2023-07-18 09:51:14 +0800

    third commit by forest

--- yrdyzfgsgz/src/com/guigu/javaClassGIt.java
+++ yrdyzfgsgz/src/com/guigu/javaClassGIt.java
@@ -7,5 +7,6 @@ public class javaClassGIt {
     public static void main(String[] args) {
         System.out.println("dafa");
         System.out.println("fafadfafg");
+        System.out.println("version333");
     }
 }
